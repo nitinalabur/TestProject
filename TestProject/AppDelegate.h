b6116b31d432864a583bb2aa42db114a883b6375
Nitin Alabur <nalabur@optoro.com>
2015-09-17 16:11:56 -0400

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Nitin Alabur on 9/17/15.
+//  Copyright © 2015 Nitin Alabur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Nitin Alabur on 9/17/15.
+//  Copyright © 2015 Nitin Alabur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
